72d27c0e8318b644254381e42ca1fb8a95370749
Z <161516388+Ziphour@users.noreply.github.com>
2025-01-28 21:47:25 +0000

    Updated Server files

---
+++ server/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(cors());
+
+// API routes
+app.use("/api/music", musicRouter);
+// app.use("/api/contact", contactRoutes);
+
+// Server listening
+const PORT = process.env.PORT || 50;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
