5493effdb20206afec89eb7e53d266b3cc8a4610
Ziphour <znpinbox@gmail.com>
2025-08-04 19:18:07 +0100

    Updated Contact form with Email JS

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -1,39 +1,84 @@
-import { useState } from "react"
+ import { useRef, useState, useEffect } from 'react';
+import emailjs from '@emailjs/browser';
+
+
 
 const ContactForm = () => {
 
-    const [name, setName] = useState('');
+      const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [subject, setSubject] = useState('');
-    const [message, setMessage] = useState('');
+    const [message, setMessage] = useState(''); 
+    const [status, setStatus] = useState('');   
 
- 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if(!formData.Title ) return ;
+    useEffect(() => {
+    emailjs.init(process.env.REACT_APP_EMAILJS_PUBLIC_KEY);
+  }, []);
+
+  
+  const sendEmail = async (e) => {
+    e.persist();
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    try {
+    setStatus('Sending')
+            const emailJsResponse = await emailjs.send(
+    process.env.REACT_APP_EMAILJS_SERVICE_ID,
+    process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+    {
+      user_name: name,
+      user_email: email,
+      message: message,
+    //   subject: subject, include later once email JS is understood
+    }
+  )
+  console.log(`EmailJS results are ${emailJsResponse.status + '' + emailJsResponse.text}`)
+  setStatus('Sent');
+  setEmail('');
+  setName('');
+  setSubject('');
+  setMessage('');
+        
+    } catch (error) {
+        console.error(error);
+        setStatus('error');
     }
 
-    <>
-    <form action="" method="POST">
-        <div>
-            <label></label>
-            <input value={name} onChange={e => setName(e.target.value)} type="text" />   
-        </div>
-         <div>
-            <label></label>
-            <input value={email} onChange={e => setEmail(e.target.value) } type="email" />   
-        </div>
-        <div>
-            <label></label>
-            <input value={subject} onChange={e => setSubject(e.target.value)} type="text" />   
-        </div>
-        <div>
-            <label></label>
-            <input value={message} onChange={e => setMessage(e.target.value)} type="text" />   
-        </div>  
-    </form>
-    </>
 
+
+
+
+
+  }
+      return(
+        <>
+            <form onSubmit={sendEmail}>
+                <div>
+                    <label></label>
+                    <input value={name} onChange={e => setName(e.target.value)} type="text" />   
+                </div>
+                <div>
+                    <label></label>
+                    <input value={email} onChange={e => setEmail(e.target.value) } type="email" />   
+                </div>
+                <div>
+                    <label></label>
+                    <input value={subject} onChange={e => setSubject(e.target.value)} type="text" />   
+                </div>
+                <div>
+                    <label></label>
+                    <input value={message} onChange={e => setMessage(e.target.value)} type="text" />   
+                </div>  
+                <button disabled={status=== 'sending'}></button>
+                {status == 'sent' && <p style={{color: green}}>Success, you will be contacted soon</p>}
+                {status == 'error' && <p style={{color: red}}>error, try again</p>}
+            </form>
+
+        </>
+      )
+
+    
 }
 
 export default ContactForm
