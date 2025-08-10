360201f75169751deb307296cdc012adad87b7b3
Ziphour <znpinbox@gmail.com>
2025-08-10 16:44:38 +0100

    Hero updated with quality tweaks- including flow and html and positioning changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,12 +9,14 @@ const HomePage = () => (
       <div className="bg-layer StarBG "></div>
       <div className="bg-layer NoiseBG "></div>
       <div className="flex flexAround heroContent">
-        <div className=" flow-1 textAlign">
-          <h2 className="lightgrey-txt">THE IT GIRL</h2>
-          <h1 className="text-clr-white">SALONI</h1>
-          <h2 className="lightgrey-txt">
-            MUSIC OUT NOW <span>&lt;</span>3
-          </h2>
+        <div className="flow-2 textAlign">
+          <div className=" flow-1 ">
+            <h2 className="lightgrey-txt">THE IT GIRL</h2>
+            <h1 className="text-clr-white">SALONI</h1>
+            <h2 className="lightgrey-txt">
+              MUSIC OUT NOW <span>&lt;</span>3
+            </h2>
+          </div>
           <div className="">
             <a href="" className="button-bg-paint padding-10" alt="">
               <button className="marginEven white-txt">Stream Here</button>
