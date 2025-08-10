cb2f471e74b17da51eaa955f538f2d91854984f4
Ziphour <znpinbox@gmail.com>
2025-08-10 16:20:15 +0100

    Updates to hero banner

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,14 +10,14 @@ const HomePage = () => (
       <div className="bg-layer NoiseBG "></div>
       <div className="flex flexAround heroContent">
         <div className=" flow-1 textAlign">
-          <h2 className="">THE IT GIRL</h2>
+          <h2 className="lightgrey-txt">THE IT GIRL</h2>
           <h1 className="text-clr-white">SALONI</h1>
-          <h2 className="">
+          <h2 className="lightgrey-txt">
             MUSIC OUT NOW <span>&lt;</span>3
           </h2>
-          <div>
-            <a href="" alt="">
-              <button className="marginEven">Stream Here</button>
+          <div className="">
+            <a href="" className="button-bg-paint padding-10" alt="">
+              <button className="marginEven white-txt">Stream Here</button>
             </a>
           </div>
         </div>
@@ -29,7 +29,7 @@ const HomePage = () => (
     </section>
     {/* Promo */}
     <section className="  yellow-bg ">
-      <div className="flex Screen-sizing-2 marginEven flexSpread ">
+      <article className="flex Screen-sizing-2 marginEven flexSpread ">
         <div className="">
           <p>
             Guess what just dropped, folks? I've got a shiny new song cover hot
@@ -43,7 +43,7 @@ const HomePage = () => (
         <div>
           <img src="./assets/Images/CD.png" />
         </div>
-      </div>
+      </article>
     </section>
   </div>
 );
